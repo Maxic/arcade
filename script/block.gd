b126b2dfca60ba8d50bf339a77b7c92a07651205
Barend <barend.poot@gmail.com>
2021-09-14 14:48:47 +0200

    gems on destroy

--- script/block.gd
+++ script/block.gd
@@ -8,8 +8,14 @@ var emerald_sprite = preload("res://assets/emerald_xs.png")
 var dirt_sprite = preload("res://assets/dirt_xs.png")
 var ruby_sprite = preload("res://assets/ruby_xs.png")
 
+var gem_diamond_scene = preload("res://scenes/gem_diamond.tscn")
+var gem_ruby_scene = preload("res://scenes/gem_ruby.tscn")
+var gem_emerald_scene = preload("res://scenes/gem_emerald.tscn")
 var collider_scene = preload("res://scenes/block_collider.tscn")
 var particles_scene = preload("res://scenes/dirt_block_particles.tscn")
+
+# global vars
+var gem_scene
 
 func _init(type):
 	# General initialization needed for all types of blocks
@@ -25,14 +31,19 @@ func _init(type):
 	# Set type 
 	if type == "dirt":
 		sprite.texture = dirt_sprite
+		gem_scene = null
 	elif type == "emerald":
 		sprite.texture = emerald_sprite
+		gem_scene = gem_emerald_scene
 	elif type == "diamond":
 		sprite.texture = diamond_sprite
+		gem_scene = gem_diamond_scene
 	elif type == "ruby":
 		sprite.texture = ruby_sprite
+		gem_scene = gem_ruby_scene
 	elif type == "rock":
 		sprite.texture = rock_sprite
+		gem_scene = null
 
 	# add children
 	add_child(sprite)
@@ -41,9 +52,16 @@ func destroy(body):
 	if body.is_in_group("drill"):
 		get_node("/root/main/shake_cam").trigger_shake = true
 		
+		# particles
 		var particles = particles_scene.instance()
 		particles.position = Vector2(global_position.x + 70, global_position.y + 70)
 		particles.emitting = true
 		get_parent().add_child(particles)
+		
+		# gem 
+		if gem_scene:
+			var gem = gem_scene.instance()
+			gem.position = Vector2(global_position.x + 70, global_position.y + 70)
+			get_parent().add_child(gem)
 		
 		queue_free()
