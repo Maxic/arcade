e112118443372182f0369820c753feb538df4d4b
davidalpha <david.alpha@gmail.com>
2021-09-14 13:38:40 +0200

    Gem images and scenes

--- script/diamond_block.gd
+++ script/diamond_block.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 # particles
 var particles_scene = preload("res://scenes/dirt_block_particles.tscn")
+var gem_scene = preload("res://scenes/gem_diamond.tscn")
 
 func _ready():
 	add_to_group("blocks")
@@ -11,8 +12,11 @@ func _on_dirt_block_body_entered(body):
 		get_node("/root/main/shake_cam").trigger_shake = true
 		
 		var particles = particles_scene.instance()
+		var gem = gem_scene.instance()
+		gem.position = Vector2(global_position.x + 70, global_position.y + 70)
 		particles.position = Vector2(global_position.x + 70, global_position.y + 70)
 		particles.emitting = true
 		get_parent().add_child(particles)
+		get_parent().add_child(gem)
 		
 		queue_free()
