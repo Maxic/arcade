037ba5c1b3b9aab3850d03945a8c1b853186a752
davidalpha <david.alpha@gmail.com>
2021-09-18 18:40:02 +0200

    deleted some things - UI improve

--- script/area_up.gd
+++ script/area_up.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
+var end_screen = load("res://scenes/end_screen.tscn")
+
 func _on_area_up_area_entered(area):
 	var block = area.get_parent()
 	if block is Block:
 		if block.type == "rock":
 			
 			GameState.dead = true
+			var end_screen_inst = end_screen.instance()
+			get_node("/root/main/").add_child(end_screen_inst)
 		else:
 			block.destroyed_by_laser = true
---
+++ script/end_screen.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var explosion = load("res://scenes/explosion.tscn") 
+var explosion_inst
+var delta_sum = 0
+
+func _physics_process(delta):
+	$black_screen.set_modulate(lerp($black_screen.get_modulate(), Color(0,0,0,1), 0.01))
+	explosion_inst = explosion.instance()
+	explosion_inst.global_position = Vector2(rand_range(100,900), rand_range(100,1800))
+	add_child(explosion_inst)
+	explosion_inst.emitting = true
+	delta_sum += delta
+	
+	if delta_sum > 3:
+		#restart game
+		queue_free()
+	
