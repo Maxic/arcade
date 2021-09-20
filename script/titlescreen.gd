f7bb3758906ebe4f16b8fb131a5759429d6b47dd
Barend <barend.poot@gmail.com>
2021-09-20 15:55:20 +0200

    titlescreenw orks

--- script/titlescreen.gd
+++ script/titlescreen.gd
@@ -7,3 +7,6 @@ func _physics_process(delta):
 	
 	if timer % 50 == 0:
 		$black_bg/TextureRect/tap_to_start_label.visible = !$black_bg/TextureRect/tap_to_start_label.visible
+	
+	if Input.is_action_just_pressed("shoot"):
+		get_tree().change_scene("res://main.tscn")
