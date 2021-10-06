1d4f72de2097608c8913f7b937d166b33c897a53
Barend <barend.poot@gmail.com>
2021-10-06 13:01:41 +0200

    bought state

--- script/buy_screen.gd
+++ script/buy_screen.gd
@@ -1,10 +1,13 @@
 extends Control
 
+# preload assets
+var bought_state_level_2 = preload("res://assets/powerup_box_drill_lvl2_bought.png")
+
 onready var level_2 = get_node("MarginContainer/v_box/level_2")
 onready var level_3 = get_node("MarginContainer/v_box/level_3")
 
 func _physics_process(delta):
-	if GameState.score > 1000:
+	if GameState.score > 1000 and Upgrades.drill_level_2 == false:
 		level_2.disabled = false
 	else:
 		level_2.disabled = true
@@ -16,6 +19,8 @@ func _physics_process(delta):
 
 func _on_level_2_pressed():
 	Upgrades.drill_level_2 = true
+	var disabled_stylebox = level_2.get_stylebox('disabled')
+	disabled_stylebox.texture = bought_state_level_2
 
 func _on_level_3_pressed():
 	Upgrades.drill_level_3 = true
